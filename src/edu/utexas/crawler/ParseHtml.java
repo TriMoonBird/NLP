3e2a47f2f92593f2f0457e05112b6ee54b9fac8a
Y. Wang <trimoonbird@gmail.com>
2015-04-16 12:54:23 -0500

    Static HTML Parser

--- src/edu/utexas/crawler/ParseHtml.java
+++ src/edu/utexas/crawler/ParseHtml.java
@@ -2,17 +2,53 @@ package edu.utexas.crawler;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class ParseHtml {
+	private static final String NEW_LINE = System.getProperty("line.separator");
+	
 	public static void main(String[] args) throws Exception{
-		String filepath = "data/facebook.html";
-		BufferedReader reader = new BufferedReader(new FileReader(filepath));
-		String str;
-		while ((str = reader.readLine()) != null) {
-			if (str.length() < 500) {
-				System.out.println(str);
+		String input = "data/facebook.html";
+		String output = "data/facebook.txt";
+		parse(input, output);
+	}
+	
+	public static void parse(String input, String output) {
+		//String authorPattern = "<span +class=\"author-name\"> <a href=.*>(.*?)</a> *</span>";
+		String reviewPattern = "<div +class=\"review-body\">(.*?)</div>";
+		String contentPattern = " *<span +class=\"review-title\">(.*?)</span> *(.*?)<div +class=\"review-link\".*";
+		Pattern reviewSection = Pattern.compile(reviewPattern);
+		Pattern reviewItem = Pattern.compile(contentPattern);
+		int reviewCount = 0;
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(input));
+			FileWriter writer = new FileWriter(output);
+			String str;
+			while ((str = reader.readLine()) != null) {
+				Matcher reviewMatcher = reviewSection.matcher(str);
+				while (reviewMatcher.find()) {
+					Matcher contentMatcher = reviewItem.matcher(reviewMatcher.group(1));
+					if (contentMatcher.matches()) {
+						String title = contentMatcher.group(1);
+						String review = contentMatcher.group(2);
+						writer.write(new ReviewItem(title, review) + NEW_LINE);
+						//System.out.println(title + " " + review);
+						++reviewCount;
+					} else {
+						writer.write("No matches" + NEW_LINE);
+						//System.out.println("No matches");
+					}
+					reviewMatcher = reviewMatcher.region(reviewMatcher.end(), reviewMatcher.regionEnd());
+				}
 			}
+			reader.close();
+			writer.close();
+			System.out.println("Review Count: " + reviewCount);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		reader.close();
 	}
 }
--- src/edu/utexas/crawler/ReviewItem.java
+++ src/edu/utexas/crawler/ReviewItem.java
@@ -2,15 +2,25 @@ package edu.utexas.crawler;
 
 class ReviewItem {
 	private String mAuthor;
+	private String mTitle;
 	private String mReview;
 	
-	public ReviewItem(String author, String review) {
+	public ReviewItem(String author, String title, String review) {
 		mAuthor = author;
+		mTitle = title;
 		mReview = review;
 	}
 	
+	public ReviewItem(String title, String review) {
+		this(null, title, review);
+	}
+	
+	public ReviewItem(String review) {
+		this(null, null, review);
+	}
+	
 	public ReviewItem() {
-		this(null, null);
+		this(null, null, null);
 	}
 	
 	public String author() {
@@ -21,11 +31,29 @@ class ReviewItem {
 		return mReview;
 	}
 	
+	public String title() {
+		return mTitle;
+	}
+	
 	public void setAuthor(String author) {
 		mAuthor = author;
 	}
 	
 	public void setReview(String review) {
 		mReview = review;
+	}
+	
+	public void setTitle(String title) {
+		mTitle = title;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		final String NEW_LINE = System.getProperty("line.separator");
+		if (mAuthor != null) str.append("Author: " + mAuthor + NEW_LINE);
+		if (mTitle != null) str.append("Title: " + mTitle + NEW_LINE);
+		if (mReview != null) str.append("Review: " + mReview + NEW_LINE);
+		return str.toString();
 	}
 }
